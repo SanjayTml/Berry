e8cc31882ed231a21da4c75975452edd8c55493c
Sanjay Timilsena <49370570+SanjayTml@users.noreply.github.com>
2023-01-05 14:18:32 +0000

    Added taking notes and getting notes events, deleting note function, attached DB for notes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ cse_id = os.environ['google-cse-id']
 import discord
 import requests
 import json
+from replit import db
 
 #Discord intent
 client = discord.Client(intents=discord.Intents.default())
@@ -19,6 +20,40 @@ limit = 1
 facts_api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(limit)
 jokes_api_url = 'https://api.chucknorris.io/jokes/random'
 google_search_url = 'https://www.googleapis.com/customsearch/v1?'
+
+
+#take a user-specific note
+def take_note(message, user_id):
+  message = '> ' + message + '\n'
+  db_key = 'notes' + user_id
+  if db_key in db.keys():
+    notes = db[db_key]
+    notes.append(message)
+    db[db_key] = notes
+    return "Message was sucessfully added to your notes!"
+  else:
+    db[db_key] = [message]
+    return "A new notekeeper was created and message was added!"
+
+
+#delete a user-specific note
+def delete_note(index, user_id):
+  db_key = 'notes' + user_id
+  notes = db[db_key]
+  if len(notes) > index:
+    del notes[index]
+    db[db_key] = notes
+
+
+#get user-specific notes
+def get_notes(user_id):
+  db_key = 'notes' + user_id
+  if db_key in db.keys():
+    notes = db[db_key]
+    format_notes = append_list(notes)
+    return format_notes
+  else:
+    return "No notes found for you. Try adding one first :)"
 
 
 #retrieve a fact using api-ninja api
@@ -59,15 +94,33 @@ def google_search(search_term, **kwargs):
       'Can not retrieve search results at this moment, please try again later.'
     )
 
+
 #Google search results parser
 def parse_results(data):
   items = data["items"]
   output = "Search Results:\n"
   # print(data)
-  for i in range(6):
-    output = output + items[i]['title'] + '\n ---- \n' + items[i]['link'] + '\n ---- \n' + items[i]['snippet'] + '\n ---- \n'
+  for i in range(4):
+    output = output + '```' + items[i]['title'] + '\n' + items[i][
+      'link'] + '\n' + items[i]['snippet'] + '```' + '\n'
   return output
 
+
+#Message parser
+def parse_message(message, term):
+  clean_messege = message.split('<', 1)[0]
+  result = clean_messege.replace(term, '', 1)
+  return result
+
+
+#Append list into message
+def append_list(list):
+  message = 'Look what I found: \n'
+  for i in range(len(list)):
+    message = message + list[i]
+  return message
+
+  
 #bot online event listener
 @client.event
 async def on_ready():
@@ -82,6 +135,7 @@ async def on_message(message):
     return
 
   if message.content.startswith('hello'):
+    print(message)
     await message.channel.send('Hello {0.author.name}!'.format(message))
 
   if 'fact' in message.content:
@@ -93,11 +147,22 @@ async def on_message(message):
     await message.channel.send(joke)
 
   if message.content.startswith('search'):
-    clean_messege = message.content.split('<', 1)[0]
-    search_term = clean_messege.replace('search', '', 1)
+    search_term = parse_message(message.content, 'search')
     print(search_term)
     search_res = google_search(search_term)
     await message.channel.send(search_res)
+
+  if message.content.startswith('takenote'):
+    note = parse_message(message.content, 'takenote')
+    print(note)
+    user_id = str(message.author.id)
+    status = take_note(note, user_id)
+    await message.channel.send(status)
+
+  if message.content.startswith('notes'):
+    user_id = str(message.author.id)
+    notes = get_notes(user_id)
+    await message.channel.send(notes)
 
 
 client.run(token)
